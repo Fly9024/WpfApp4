451bd4fdc8881be993269afbea5d22333a3d5852
fly9024 <fly9024@gmail.com>
2021-10-23 16:15:14 +0300

    пагинация (распределение списка по страницам)

--- WpfApp4/pages/PageUsersLisl.xaml.cs
+++ WpfApp4/pages/PageUsersLisl.xaml.cs
@@ -21,6 +21,7 @@ namespace WpfApp4.pages
     public partial class PageUsersLisl : Page
     {
         List<users> users;
+        List<users> lu1;
         public PageUsersLisl()
         {
             InitializeComponent();
@@ -30,6 +31,7 @@ namespace WpfApp4.pages
             lbGenderFilter.ItemsSource = BaseConnect.BaseModel.genders.ToList();
             lbGenderFilter.SelectedValuePath = "id";
             lbGenderFilter.DisplayMemberPath = "gender";
+            lu1 = users;
         }
 
         private void lbTraits_Loaded(object sender, RoutedEventArgs e)
@@ -43,7 +45,7 @@ namespace WpfApp4.pages
 
         private void Filter(object sender, RoutedEventArgs e)
         {
-            List<users> lu1=users;
+            
             //фильтр по количеству            
             try
             {
@@ -69,7 +71,7 @@ namespace WpfApp4.pages
                 lu1 = lu1.Where(x => x.name.Contains(txtNameFilter.Text)).ToList();
             }
 
-            lbUsersList.ItemsSource = lu1;
+            lbUsersList.ItemsSource = lu1;// возвращаем результат в виде списка, к которому применялись активные фильтры
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
@@ -79,6 +81,99 @@ namespace WpfApp4.pages
             txtNameFilter.Text = "";//сбрасываем фильтр на строку
             txtOT.Text = "";
             txtDO.Text = "";
+        }
+        int currentpage = 1;
+        private void GoPage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock tb = (TextBlock)sender;//определяем, какой текстовый блок был нажат
+            int countlist = users.Count;//определяем общее колисество пользователей
+            int countpage = Convert.ToInt32(txtPageCount.Text);//количество записей на странице
+            int countpages = countlist / countpage;//количество страниц
+            //изменение номера страници при нажатии на кнопку
+            switch (tb.Uid)// в зависимости от нажатой кнопки
+            {
+                case "prev":
+                    currentpage--;
+                    break;
+                case "1":
+                    if (currentpage < 3) currentpage = 1;
+                    else if (currentpage > countpages - 2) currentpage = countpages - 4;
+                    else currentpage -= 2;
+                    break;
+                case "2":
+                    if (currentpage < 3) currentpage = 2;
+                    else if (currentpage > countpages - 2) currentpage = countpages - 3;
+                    else currentpage --;
+                    break;
+                case "3":
+                    if (currentpage < 3) currentpage = 3;
+                    else if (currentpage > countpages - 2) currentpage = countpages - 2;                    
+                    break;
+                case "4":
+                    if (currentpage < 3) currentpage = 4;
+                    else if (currentpage > countpages - 2) currentpage = countpages - 1;
+                    else currentpage ++;
+                    break;
+                case "5":
+                    if (currentpage < 3) currentpage = 5;
+                    else if (currentpage > countpages - 2) currentpage = countpages;
+                    else currentpage += 2;
+                    break;
+                case "next":
+                    currentpage++;
+                    break;
+                default:
+                    currentpage = 1;
+                    break;
+            }
+            //ограничение на выход за пределы страницы
+            if (currentpage <1) currentpage = 1;
+            if (currentpage >= countpages) currentpage = countpages;
+
+            //изменение номера страницы в значении текстового поля (отрисовка)
+            if (currentpage < 3)
+            {
+                txt1.Text = " 1 ";
+                txt2.Text = " 2 ";
+                txt3.Text = " 3 ";
+                txt4.Text = " 4 ";
+                txt5.Text = " 5 ";
+            }
+            else if (currentpage > countpages - 2)
+            {
+                txt1.Text = " " + (countpages - 4).ToString() + " ";
+                txt2.Text = " " + (countpages - 3).ToString() + " ";
+                txt3.Text = " " + (countpages - 2).ToString() + " ";
+                txt4.Text = " " + (countpages - 1).ToString() + " ";
+                txt5.Text = " " + (countpages).ToString() + " ";
+            }
+            else
+            {
+                txt1.Text = " " + (currentpage - 2).ToString() + " ";
+                txt2.Text = " " + (currentpage - 1).ToString() + " ";
+                txt3.Text = " " + (currentpage).ToString() + " ";
+                txt4.Text = " " + (currentpage + 1).ToString() + " ";
+                txt5.Text = " " + (currentpage + 2).ToString() + " ";
+            }
+            txtCurrentPage.Text = "Текущая страница: " + (currentpage).ToString();
+
+            //изменение списка пользователей
+            lu1 = users.Skip(currentpage * countpage - countpage).Take(countpage).ToList();
+            lbUsersList.ItemsSource = lu1;
+        }
+
+        private void txtPageCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                lu1 = users.Take(Convert.ToInt32(txtPageCount.Text)).ToList();
+                lbUsersList.ItemsSource = lu1;
+            }
+            catch
+            {
+
+            }
+            
         }
     }
 }
