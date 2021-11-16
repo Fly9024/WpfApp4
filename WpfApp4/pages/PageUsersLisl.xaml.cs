47e8b51803605638f74143381e789f2eb440f053
fly9024 <fly9024@gmail.com>
2021-11-16 11:17:43 +0300

    вставка и добавления изображения в базу данных

--- WpfApp4/pages/PageUsersLisl.xaml.cs
+++ WpfApp4/pages/PageUsersLisl.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -129,24 +132,64 @@ namespace WpfApp4.pages
 
         private void UserImage_Loaded(object sender, RoutedEventArgs e)
         {
-            Image IMG = sender as Image;
+            System.Windows.Controls.Image IMG = sender as System.Windows.Controls.Image;
             int ind = Convert.ToInt32(IMG.Uid);
-            users U = BaseConnect.BaseModel.users.FirstOrDefault(x => x.id == ind);
-            BitmapImage BI;
-            switch (U.gender)
-            {
-                case 1:
-                    BI = new BitmapImage(new Uri(@"/images/male.jpg", UriKind.Relative));
-                    break;
-                case 2:
-                    BI = new BitmapImage(new Uri(@"/images/female.jpg", UriKind.Relative));
-                    break;
-                default:
-                    BI = new BitmapImage(new Uri(@"/images/other.jpg", UriKind.Relative));
-                    break;
+            users U = BaseConnect.BaseModel.users.FirstOrDefault(x => x.id == ind);//запись о текущем пользователе
+            usersimage UI = BaseConnect.BaseModel.usersimage.FirstOrDefault(x => x.id_user == ind);//получаем запись о картинке для текущего пользователя
+            BitmapImage BI=new BitmapImage();
+            if (UI != null)//если для текущего пользователя существует запись о его катринке
+            { 
+                if (UI.path!=null)//если присутствует путь к картинке
+                {
+                    BI = new BitmapImage(new Uri(UI.path, UriKind.Relative));
+                }
+                else//если присутствуют двоичные данные
+                {
+                    BI.BeginInit();//начать инициализацию BitmapImage (для помещения данных из какого-либо потока)
+                    BI.StreamSource = new MemoryStream(UI.image);//помещаем в источник данных двоичные данные из потока
+                    BI.EndInit();//закончить инициализацию
+                }
             }
-
+            else//если в базе не содержится картинки, то ставим заглушку
+            {
+                switch (U.gender)//в зависимости от пола пользователя устанавливаем ту или иную картинку
+                {
+                    case 1:
+                        BI = new BitmapImage(new Uri(@"/images/male.jpg", UriKind.Relative));
+                        break;
+                    case 2:
+                        BI = new BitmapImage(new Uri(@"/images/female.jpg", UriKind.Relative));
+                        break;
+                    default:
+                        BI = new BitmapImage(new Uri(@"/images/other.jpg", UriKind.Relative));
+                        break;
+                }
+            }              
             IMG.Source = BI;//помещаем картинку в image
+        }
+
+        private void BtmAddImage_Click(object sender, RoutedEventArgs e)
+        {
+            Button BTN = (Button)sender;
+            int ind = Convert.ToInt32(BTN.Uid);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".jpg"; // задаем расширение по умолчанию
+            openFileDialog.Filter = "Изображения |*.jpg;*.png"; // задаем фильтр на форматы файлов
+            var result = openFileDialog.ShowDialog();
+            if (result == true)//если файл выбран
+            {
+                System.Drawing.Image UserImage = System.Drawing.Image.FromFile(openFileDialog.FileName);//создаем изображение
+                ImageConverter IC = new ImageConverter();//конвертер изображения в массив байт
+                byte[] ByteArr = (byte[])IC.ConvertTo(UserImage, typeof(byte[]));//непосредственно конвертация
+                usersimage UI = new usersimage() { id_user = ind, image = ByteArr };//создаем новый объект usersimage
+                BaseConnect.BaseModel.usersimage.Add(UI);//добавляем его в модель
+                BaseConnect.BaseModel.SaveChanges();//синхронизируем с базой
+                MessageBox.Show("картинка пользователя добавлена в базу");
+            }
+            else
+            {
+                MessageBox.Show("операция выбора изображения отменена");
+            }
         }
     }
 }
